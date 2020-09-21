aa67f9f9eff989c639ff2a7712a0321bf5ceee73
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 09:24:44 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,6 @@
+var tgl=2, a=37
+
+console.log("dengan console log biasa nilai fisika",a/tgl)
+console.log("dengan Math floor",Math.floor(a/tgl))
+console.log("dengan Math ceil",Math.ceil(a/tgl))
+console.log("dengan Math round",Math.round(a/tgl))
